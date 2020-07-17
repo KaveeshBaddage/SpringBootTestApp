bd773fd9d1da98a4f1e67cd23bf1f188d14e7510
kaveeshab <kbaddage98@gmail.com>
2020-07-18 00:15:22 +0530

    Add a package to add constants used in the application

---
+++ src/main/java/com/kaveesh/testApp/constant/AppConstants.java
@@ -0,0 +1,7 @@
+package com.kaveesh.testApp.constant;
+
+public class AppConstants {
+    public static final String REQUEST_ID = "REQUEST_ID";
+    public static final String REQUEST_AUDIT_ID = "REQUEST_AUDIT_ID";
+    public static final String REQUEST_START_TIME = "REQUEST_START_TIME";
+}
