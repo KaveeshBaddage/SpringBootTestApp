a26ebe958bf6a76f26619fc614168647c578a02f
kaveeshab <kbaddage98@gmail.com>
2020-07-18 00:17:15 +0530

    Register RequestAuditInterceptor

--- src/main/java/com/kaveesh/testApp/configuration/InterceptorConfig.java
+++ src/main/java/com/kaveesh/testApp/configuration/InterceptorConfig.java
@@ -1,14 +1,23 @@
 package com.kaveesh.testApp.configuration;
 
+import com.kaveesh.testApp.filter.RequestAuditInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+/*
+* This class holds configuration related to all interceptors in application
+* */
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
+    @Autowired
+    RequestAuditInterceptor requestAuditInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry){
-
+        registry.addInterceptor(requestAuditInterceptor);
     }
 }
