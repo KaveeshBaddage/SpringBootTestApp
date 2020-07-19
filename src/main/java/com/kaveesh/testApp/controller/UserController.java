a942d1a8c3009348c03b9562d871e66972163440
kaveeshab <kbaddage98@gmail.com>
2020-07-19 22:29:37 +0530

    Add User Controller

--- src/main/java/com/kaveesh/testApp/controller/UserController.java
+++ src/main/java/com/kaveesh/testApp/controller/UserController.java
@@ -1,9 +1,19 @@
 package com.kaveesh.testApp.controller;
 
+import com.kaveesh.testApp.dto.user.AdminCreateRequestDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
-@RequestMapping("/user/v1")
+@RequestMapping("/user/")
 public class UserController {
+
+    @PostMapping("admin/create")
+    public void createAdminAccount(@Valid @RequestBody AdminCreateRequestDTO request){
+        System.out.println("XXXXXXXXXXXXXXXXXXXx");
+    }
 }
